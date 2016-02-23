71aa09a7557db885e73bb184c6d95e767ab40987
Lauri Härsilä <murgo@iki.fi>
2016-02-23 03:34:26 +0200

    Merged foreground music player service

--- app/src/main/java/fi/iki/murgo/iltasoitto/app/MusicPlayerService.java
+++ app/src/main/java/fi/iki/murgo/iltasoitto/app/MusicPlayerService.java
@@ -1,35 +1,102 @@
 package fi.iki.murgo.iltasoitto.app;
 
-import android.app.IntentService;
-import android.content.Context;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-public class MusicPlayerService extends IntentService {
-    private static MediaPlayer mediaPlayer;
-
-    public MusicPlayerService() {
-        super("MusicPlayerService");
-    }
+public class MusicPlayerService extends Service {
+    private static final int NOTIFICATION_ID = 0x666;
+    private static final String STARTED = "STARTED";
+    private MediaPlayer mediaPlayer;
+    private Intent startIntent;
+    private Thread backgroundThread;
 
     @Override
-    protected void onHandleIntent(final Intent intent) {
-        Context context = getApplicationContext();
+    public void onCreate() {
+        super.onCreate();
 
-        mediaPlayer = MediaPlayer.create(context, R.raw.iltasoitto);
+        mediaPlayer = MediaPlayer.create(this, R.raw.sonni);
+
         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
-                Log.i("iltasoitto", "Iltasoitto played succesfully.");
-                mediaPlayer.release();
-                mediaPlayer = null;
+                Log.i("iltasonni", "Iltasonni played succesfully.");
 
-                HarjuLauncher.completeWakefulIntent(intent);
+                stopSelf();
             }
         });
-        mediaPlayer.start();
 
-        AlarmSetter.checkAlarm(context);
+        backgroundThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mediaPlayer.start();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mediaPlayer != null) {
+            mediaPlayer.setOnCompletionListener(null);
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        if (backgroundThread != null) {
+            Thread dummy = backgroundThread;
+            backgroundThread = null;
+            dummy.interrupt();
+        }
+
+        if (startIntent != null) {
+            HarjuLauncher.completeWakefulIntent(startIntent);
+            startIntent = null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent.getBooleanExtra(STARTED, true)) {
+            startIntent = intent;
+
+            startForeground(NOTIFICATION_ID, getNotification());
+
+            if (!mediaPlayer.isPlaying()) {
+                backgroundThread.start();
+            }
+
+            AlarmSetter.checkAlarm(this);
+        } else {
+            stopSelf();
+        }
+
+        return START_STICKY;
+    }
+
+    private Notification getNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        builder.setSmallIcon(R.mipmap.ic_launcher).setContentTitle(getText(R.string.notification_title)).setContentText(getText(R.string.notification_text)).setWhen(System.currentTimeMillis());
+        Intent service = new Intent(this, MusicPlayerService.class);
+        service.putExtra(STARTED, false);
+        PendingIntent pi = PendingIntent.getService(this, 0, service, 0);
+
+        builder.setContentIntent(pi);
+
+        return builder.build();
     }
 }
